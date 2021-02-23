d7a5dc208b5aa2b7bb22e5f367f685481aa58894
Malena de Juan <malena.dj4@gmail.com>
2021-02-23 18:59:34 +0100

    analizar frase sin acentos ni símbolos

--- scripts.js
+++ scripts.js
@@ -1,9 +1,17 @@
-/* function esPalindromo(cadena) {
+function esPalindromo(cadena) {
+  const cadenaLimpia = cadena.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[.,:;()_?¿!¡-\s]/g, "");
+  if (cadenaLimpia.toLowerCase()
+    .split("")
+    .reverse()
+    .join("") === cadenaLimpia.toLowerCase()) { return true; } else { return false; }
+}
 
-} */
-
-const miCadena = "hola";
-
-const esPalindromo = cadena => cadena.toLowerCase().split("").reverse().join("") === cadena.toLowerCase();
+const miCadena = "hòla ?alóh!";
 
 console.log(esPalindromo(miCadena));
+
+/* const esPalindromo = cadena => cadena
+  .toLowerCase()
+  .split("")
+  .reverse()
+  .join("") === cadena.toLowerCase();  */
