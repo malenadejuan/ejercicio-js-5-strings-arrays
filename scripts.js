d3d6e1cc739c5eec3807d164d14a739afdc342a8
Malena de Juan <malena.dj4@gmail.com>
2021-02-24 10:20:09 +0100

    funcion a arrow y frase de salida a console

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
-function esPalindromo(cadena) {
+const esPalindromo = cadena => {
   const cadenaLimpia = cadena.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[.,:;()_?¿!¡-\s]/g, "");
   return cadenaLimpia.toLowerCase()
     .split("")
     .reverse()
     .join("") === cadenaLimpia.toLowerCase();
-}
+};
 
 const miCadena = "hòla ?alóh!";
 
-console.log(esPalindromo(miCadena));
+console.log(`La frase ${esPalindromo(miCadena) ? "" : "no "}es un palíndromo`);
 
 /* const esPalindromo = cadena => cadena
   .toLowerCase()
